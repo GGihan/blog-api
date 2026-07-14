d23ff53f7132bba495a4027ad7ee8ca562190ee1
ggihan <140435642+GGihan@users.noreply.github.com>
2026-07-14 21:30:47 +0200

    Add(backend) folder structure, prisma models setup

---
+++ backend/lib/supabase.js
@@ -0,0 +1,9 @@
+// import 'dotenv/config';
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export { supabase };
